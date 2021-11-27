bf60d20377bbd285f5fea02b347b609f170e8d44
tomi <dev.dreissiger@gmail.com>
2021-11-27 14:53:02 +0100

    tag questions work 7

--- src/components/TagQuestions.jsx
+++ src/components/TagQuestions.jsx
@@ -18,11 +18,11 @@ export default function TagQuestions() {
 
             <SubTitle>A lényeg:</SubTitle>
 
-            <Row decor>
+            <Row bg>
                 <span>You are Hungarian, aren’t you?</span>
                 <span>Te magyar vagy, ugye?</span>
             </Row>
-            <Row decor last>
+            <Row bg last>
                 <span>The boys can't beat us, can they?</span>
                 <span>A fiúk nem tudnak megverni minket, ugye?</span>
             </Row>
@@ -38,14 +38,14 @@ export default function TagQuestions() {
             <Row>
                 <span>1. "I’m" -es kezdés esetén állító mondatban:</span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>I’m good, aren’t I? <br /> (NEM "amn’t I"!)</span>
                 <span>Jó vagyok, ugye?</span>
             </Row>
             <Row>
                 <span>De:</span>
             </Row>
-            <Row decor last>
+            <Row bg last>
                 <span>I'm not good, am I?</span>
                 <span>Nem vagyok jó, ugye?</span>
             </Row>
@@ -53,7 +53,7 @@ export default function TagQuestions() {
             <Row>
                 <span>2. Saját magunk felszólítása esetén:</span>
             </Row>
-            <Row decor last>
+            <Row bg last>
                 <span>Let’s (not) go there, shall we?</span>
                 <span>(Ne) menjünk oda, oké?</span>
             </Row>
@@ -61,11 +61,11 @@ export default function TagQuestions() {
             <Row>
                 <span>3. Mások felszólítása esetén:</span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>Stop it, will you / (won’t you)?</span>
                 <span>Hagyd abba, oké?</span>
             </Row>
-            <Row decor last>
+            <Row bg last>
                 <span>Don't stop it, will you?</span>
                 <span>Ne hagyd abba, oké?</span>
             </Row>
@@ -73,7 +73,7 @@ export default function TagQuestions() {
             <Row>
                 <span>4. Nobody/ Everybody/ Somebody/ Anybody/ No one/ Everyone/ Someone/ Anyone (*):</span>
             </Row>
-            <Row decor last>
+            <Row bg last>
                 <span>Everybody likes him, don’t they?</span>
                 <span>Mindenki szereti őt, ugye?</span>
             </Row>
@@ -85,7 +85,7 @@ export default function TagQuestions() {
             <Row>
                 <span>5. Nothing/ Everything/ Something/ Anything (*):</span>
             </Row>
-            <Row decor last>
+            <Row bg last>
                 <span>Everything is black, isn’t it?</span>
                 <span>Minden fekete, ugye?</span>
             </Row>
@@ -100,18 +100,18 @@ export default function TagQuestions() {
             <Row>
                 <span>- pl. "nobody" vagy "never":</span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>Nobody can help me, can they?</span>
                 <span>Senki nem tud nekem segíteni, ugye?</span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>Joe never sleeps, does he?</span>
                 <span>Joe sose alszik, ugye?</span>
             </Row>
             <Row>
                 <span>- esetleg "hardly”, "barely” vagy "scarcely” (jelentésük: "alig"):</span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>He can hardly speak, can he?</span>
                 <span>Alig tud beszélni, ugye?</span>
             </Row>
@@ -125,11 +125,11 @@ export default function TagQuestions() {
                     hogy egynél több alany és állítmány van, tehát el kell döntenünk, hogy az "ugye" melyikre vonatkozik:
                 </span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>I don’t think Peter is rich, do I?</span>
                 <span>Nem hiszem, hogy Péter gazdag, ugye? (... vagy hiszem?)</span>
             </Row>
-            <Row decor last>
+            <Row bg last>
                 <span>I don't think Peter is rich, is he?</span>
                 <span>Nem hiszem, hogy Péter gazdag, ugye? (... vagy gazdag?)</span>
             </Row>
@@ -140,14 +140,14 @@ export default function TagQuestions() {
                     akkor azok az alany elé kerülnek, ha nem vonjuk össze, akkor a "not" az alany mögé kerül:
                 </span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>I should go, shouldn’t I?</span>
                 <span>Mennem kéne, nem?</span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>=</span>
             </Row>
-            <Row decor last>
+            <Row bg last>
                 <span>I should go, should I not?</span>
                 <span>Mennem kéne, nem?</span>
             </Row>
@@ -157,11 +157,11 @@ export default function TagQuestions() {
                     9. Egyszerű jelenben és múltban állító mondatokban alapból nincs segédige. Ilyenkor az "ugye" így működik:
                 </span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>She hates me, doesn't she?</span>
                 <span>Utál engem, ugye?</span>
             </Row>
-            <Row decor last>
+            <Row bg last>
                 <span>My brother broke that dude's leg, didn't he?</span>
                 <span>A bátyám eltörte annak a fickónak a lábát, ugye?</span>
             </Row>
@@ -171,14 +171,14 @@ export default function TagQuestions() {
                     10. "there be"-s mondatok:
                 </span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>I should go, shouldn’t I?</span>
                 <span>Mennem kéne, nem?</span>
             </Row>
-            <Row decor>
+            <Row bg>
                 <span>=</span>
             </Row>
-            <Row decor last>
+            <Row bg last>
                 <span>There is a cat under the table, isn’t there?</span>
                 <span>Van egy macska az asztal alatt, ugye?</span>
             </Row>
--- src/styled-components/Row.jsx
+++ src/styled-components/Row.jsx
@@ -4,7 +4,7 @@ export const Row = styled.div`
     display: flex;
     justify-content: space-between;
     margin-bottom: ${({last}) => last ? '1rem' : '0.5rem'};
-    background-color: ${({decor}) => decor ? 'var(--decor)' : 'var(--day)'};
+    background-color: ${({bg}) => bg ? 'var(--light-day)' : 'var(--day)'};
     padding: ${({decor}) => decor ? '0.5rem' : '0'};
     border-radius: 0.25rem;
 
